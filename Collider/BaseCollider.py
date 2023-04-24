0efe010c69bea43d62f95adfb325a9cd269fa5c4
0gl20shk0sbt36 <91115385+0gl20shk0sbt36@users.noreply.github.com>
2023-04-24 09:27:13 +0000

    Pending changes exported from your codespace

--- Collider/BaseCollider.py
+++ Collider/BaseCollider.py
@@ -3,5 +3,6 @@ class BaseCollider:
     def __init__(self, point=(0, 0), opposite=False):
         self.point = point
         self.opposite = opposite
-    
+
     def collision(self, point):
+        pass
